c981d66c289a2a29e572e7961fd7ca472a455646
Anand kumar <anandkumar.triden@gmail.com>
2023-07-01 14:56:43 +0530

    server update

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,5 @@
 //export const server = "https://ecommerce-market-project.herokuapp.com/api/v2";
 
 //export const backend_url = "https://ecommerce-market-project.herokuapp.com/";
-export const server = "https://shopebackend.onrender.com/api/v2";
-export const backend_url = "https://shopebackend.onrender.com/";
+export const server = "https://shy-rose-tadpole-tie.cyclic.app/api/v2";
+export const backend_url = "https://shy-rose-tadpole-tie.cyclic.app/";
