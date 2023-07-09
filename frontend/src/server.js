c8c743b6c00afc7421f0b488854973a01a0bc545
Anand kumar <anandkumar.triden@gmail.com>
2023-07-09 12:26:14 +0530

    SERVER UPDATE FOR VERCEL

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,5 @@
 //export const server = "https://ecommerce-market-project.herokuapp.com/api/v2";
 
 //export const backend_url = "https://ecommerce-market-project.herokuapp.com/";
-export const server = "https://shy-rose-tadpole-tie.cyclic.app/api/v2";
-export const backend_url = "https://shy-rose-tadpole-tie.cyclic.app/";
+export const server = "https://shopebackend-qwiq.vercel.app/api/v2";
+export const backend_url = "https://shopebackend-qwiq.vercel.app/";
